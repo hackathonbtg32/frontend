6395d5bbf6ec592da0800a7282fa433a907e75cc
Raul Reis <raul.soares@usp.br>
2022-09-25 12:43:59 -0300

    Criação dos componentes da tela de histórico

--- pages/history.vue
+++ pages/history.vue
@@ -1,14 +1,24 @@
 <template>
+
+<section class="p-4">
     <h6 class="title">Histórico</h6>
+
+    <hr/>
+
+    <HistoryCard/>
+
+</section>
+
 </template>
 <script lang="ts">
 import Vue from "vue";
+import HistoryCard from "~/components/HistoryCard.vue";
 
 export default Vue.extend({
     name: "HistoryPage",
     setup() {
-        
     },
+    components: { HistoryCard }
 })
 </script>
 
